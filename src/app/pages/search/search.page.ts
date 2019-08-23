9a10e11b686e6fd7df2423f3b1b75017b8fdbb57
Rafael Requião <rafaelfrequiao@gmail.com>
2019-08-23 17:10:46 -0300

    Falta pouco...o response tá bugando e eu preciso escrever detalhes

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -14,15 +14,19 @@ import { delay } from 'rxjs/operators';
 
 export class SearchPage implements OnInit, OnDestroy {
 
-  response: Observable<any>;
+  search: Observable<any>;
+  results: Observable<any>;
+
   searchTerm: ''; // string = '' ?
   type: SearchType = SearchType.characters;
 
   searchSubscription: Subscription;
+  resultsSubscription: Subscription;
 
   searchObserver = {
     next: x => {
-      console.log('searchObserver got a next value: ' + x);
+      console.log('searchObserver got a next value: ');
+      console.log(x);
     },
     error: err => {
       console.error('searchObserver got an error: ' + err);
@@ -30,11 +34,29 @@ export class SearchPage implements OnInit, OnDestroy {
     },
     complete: () => {
       console.log('searchObserver got a complete notification');
+      this.results = this.heroService.results; // because if it's complete: no errors occurred
       delay(3000); // I don't think it's working
       this.loadingController.dismiss();
     }
   };
 
+  resultsObserver = {
+    /*
+    next: x => {
+      console.log('resultsObserver got a next value:');
+      console.log(x);
+    },
+    */
+    error: err => {
+      console.error('resultsObserver got an error: ' + err);
+      this.presentAlert('Error', 'An error occurred while processing your request.');
+    },
+    complete: () => {
+      console.log('resultsObserver got a complete notification');
+      // delay(3000); // I don't think it's working
+      // this.loadingController.dismiss();
+    }
+  };
 
   /**
    * Constructor of our first page
@@ -49,19 +71,31 @@ export class SearchPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     console.log('ngOnInit: search page!');
+    // changed them on the service!
+    // this.heroService.response = new Observable();
+    // this.heroService.results = new Observable();
 
   }
 
   ngOnDestroy() {
     this.searchSubscription.unsubscribe();
+    this.resultsSubscription.unsubscribe();
   }
 
   searchChanged() {
     // Call our service function which returns an Observable
-    this.response = this.heroService.searchData(this.searchTerm, this.type);
+    this.search = this.heroService.searchData(this.searchTerm, this.type);
+    // this.results = this.heroService.results; // not here!
+
+    // I think I may change that
+    console.log('searchChanged from search.page ! Outputting this.search:');
+    console.log(this.search);
 
     // Create observer object, outside this function.
-    this.searchSubscription = this.response.subscribe(this.searchObserver);
+    this.searchSubscription = this.search.subscribe(this.searchObserver);
+    this.resultsSubscription = this.results.subscribe(this.resultsObserver);
+
+    // now spin up the loader
     this.presentLoadingWithOptions();
 
   }
@@ -73,7 +107,7 @@ export class SearchPage implements OnInit, OnDestroy {
       message: 'Please wait...',
       translucent: true,
       showBackdrop: true,
-      backdropDismiss: false
+      backdropDismiss: true // may I change this?
       // cssClass: 'custom-class custom-loading'
     });
     return await loading.present();
@@ -89,5 +123,36 @@ export class SearchPage implements OnInit, OnDestroy {
   }
 
   // got more functions?
+
+  previousResults() {
+    console.log('previousResults: memes');
+
+    if (this.heroService.offset === 0 ) { // stop changing the offset!
+      console.log('previousResults: offset = 0 !');
+      this.heroService.offset += 10; // compensation...
+
+    } else {
+      this.heroService.offset -= this.heroService.limit; // skip through more data using offsets!
+
+      // fire up an event?
+      this.searchChanged();
+    }
+
+  }
+
+  nextResults() {
+    console.log('nextResults: memes');
+
+    if (this.heroService.offset === this.heroService.total ) { // stop getting data!
+      console.log('nextResults: offset = total-offset !');
+      this.heroService.total -= 10; // compensation...
+
+    } else {
+      this.heroService.offset += this.heroService.limit; // skip through more offsets!
+
+      // fire up an event?
+      this.searchChanged();
+    }
+  }
 
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, empty } from 'rxjs';
+import { Observable, empty, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Md5 } from 'ts-md5/dist/md5';
 
@@ -37,6 +37,17 @@ export class HeroService {
   // hash = Md5.hashStr(this.ts + 'ebd407c102ea3f1262b8dd370cfa04d4a132a867d8b23f3429d72898aaffd1a321761b4a');
   hash = Md5.hashStr(this.ts + this.privateKey + this.publicKey , false);
 
+  // accordingly to this project's requirements
+  limit = 10;
+  offset = 0; // we need to modify this using buttons! - take care of not getting negative values!
+  total = 0;
+
+  // may I change them to Observables?
+  response: Observable<any>;
+  results: Observable<any>; // or Array?
+
+  attributionText: string;
+
   /**
    * Constructor of the Service with Dependency Injection
    * @param http The standard Angular HttpClient to make requests
@@ -52,16 +63,38 @@ export class HeroService {
 
   searchData(title: string, type: SearchType): Observable<any> {
 
+    console.log('searchData: title = ' + title);
+
     if (title.length <= 0) {
       console.log(`Busca vazia para ${type}`);
       return ; // nothing ? not even an empty Array?
 
     } else {
-      return this.http.get(`
-        ${this.url}${type}?${QueryType[type]}=${encodeURI(title)}&ts=${this.ts}&apikey=${this.publicKey}&hash=${this.hash}
-      `)
+
+      return this.http.get(`${this.url}${type}?${QueryType[type]}=${encodeURI(title)}` +
+        `&limit=${this.limit}&offset=${this.offset}` +
+        `&ts=${this.ts}&apikey=${this.publicKey}&hash=${this.hash}`
+      )
       .pipe(
-        map(response => response[`data`][`results`])
+        map(response => {
+          // what to do with the received response?
+          console.log('searchData: Got an response!');
+          console.log(response);
+
+          this.total = response[`data`][`total`];
+          this.attributionText = response[`attributionText`];
+
+          // create an Observable from the Array of Objects in response.data.results
+          const stuff = response[`data`][`results`]; // since response isn't declared: we need to pass the key names in Array-like fashion
+          if (stuff.length > 0) {
+            this.results = from(stuff);
+          } else {
+            this.results = from([]);
+          }
+
+          console.log('heroService: changed some directives.');
+
+        })
       );
     }
 
@@ -80,6 +113,10 @@ export class HeroService {
       .pipe(
         map(response => response[`data`][`results`][0])  // catch the first object from the array - we match an ID !
       );
+  }
+
+  saveResults(res: any) { // of what data type?
+
   }
 
 }
